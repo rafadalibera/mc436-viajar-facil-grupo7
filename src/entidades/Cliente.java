bd949fd3ac19f50667c17a2fc338e5338342bb3b
Guilherme Pinto Goncalves <ra091429@cure.lab.ic.unicamp.br>
2011-11-23 15:19:33 -0200

    getters para Cliente.

--- src/entidades/Cliente.java
+++ src/entidades/Cliente.java
@@ -3,14 +3,14 @@ package entidades;
 import java.util.Vector;
 
 class Cliente {
-    int id;
-    int numero_cartao;
-    String nome;
-    String senha;
-    String rg;
-    String cpf;
-    String email;
-    Vector<String> telefones;
+    private int id;
+    private int numero_cartao;
+    private String nome;
+    private String senha;
+    private String rg;
+    private String cpf;
+    private String email;
+    private Vector<String> telefones;
 
     public Cliente(int id, int numero_cartao, String nome, String senha,
                    String rg, String cpf, String email,
@@ -27,5 +27,45 @@ class Cliente {
 
     public static void main(String[] args) {
         return;
+    }
+
+    /* Retorna o número de identificação deste cliente. */
+    int getId() {
+        return this.id;
+    }
+
+    /* Retorna o número do cartão deste cliente. */
+    int getNumeroCartao() {
+         return this.numero_cartao;
+    }
+
+    /* Retorna o nome deste cliente. */
+    String getNome() {
+        return this.nome;
+    }
+
+    /* Retorna a senha deste cliente. */
+    String getSenha() {
+        return this.senha;
+    }
+
+    /* Retorna o rg deste cliente. */
+    String getRg() {
+        return this.rg;
+    }
+
+    /* Retorna o cpf deste cliente. */
+    String getCpf() {
+        return this.cpf;
+    }
+
+    /* Retorna o email deste cliente. */
+    String getEmail() {
+        return this.email;
+    }
+
+    /* Retorna o telefone deste cliente. */
+    getTelefones() {
+        return this.telefones;
     }
 }
