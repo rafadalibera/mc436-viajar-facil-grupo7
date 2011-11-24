689edd4b3bae51fff850aba46ac0426518994707
Rafael Soares Padilha <ra092735@iron.lab.ic.unicamp.br>
2011-11-24 14:38:30 -0200

    Classes PagamentoDeposito e PagamentoCartao feitas

---
+++ src/entidades/PagamentoDeposito.java
@@ -0,0 +1,30 @@
+package entidades;
+
+public class PagamentoDeposito extends Pagamento {
+	private String banco;
+	private int agencia;
+	private int conta;	
+	
+	//Construtor Basico //
+	PagamentoDeposito(Pacote pacote,double preco,int codigoPagamento,String banco,
+			          int agencia, int conta)
+	{
+		super(pacote,preco,codigoPagamento);
+		this.agencia = agencia;
+		this.conta = conta;
+		this.banco = banco;
+	}
+
+	///////////////// GETTERS ///////////////////////
+	public String getBanco() {
+		return banco;
+	}
+
+	public int getAgencia() {
+		return agencia;
+	}
+
+	public int getConta() {
+		return conta;
+	}
+}
