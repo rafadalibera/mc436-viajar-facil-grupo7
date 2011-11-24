7d7648846e376bfa28ad8583bb261118fdc3fd4b
Guilherme Pinto Goncalves <ra091429@sabbath.lab.ic.unicamp.br>
2011-11-24 17:30:09 -0200

    Adicionei um gerador de código de DAO.
    Para que isso serve
    -------------------
    
    Uma vez que configuramos a persistência dos nossos objetos com o Hibernate,
    ainda falta uma interface mais alto nível para acessar esses objetos.
    
    O Hibernate torna possível salvar um objeto, restaurá-lo e até fazer queries no
    banco de dados, mas seria interessante trabalhar com coisas do tipo "me dê todos
    os clientes com este nome" sem ter que escrever uma query em SQL -- ou seja,
    funcionalidades de DAO.
    
    No entanto, o código para definir os DAOs é extremamente repetitivo e difícil
    reaproveitar entre classes.
    
    Por isso, um gerador de código. A ideia é: você passa para ele os campos pelos
    quais deseja buscar objetos no banco de dados, e ele gera o código que faz essas
    buscas e que salva os objetos.
    
    Como funciona
    -------------
    
    Basta definir um arquivo NomeDaClasse.dao, no seguinte formato:
    
    NomeDaTabela (configurada no Hibernate)
    Tipo Campo (tipo do campo em Java)
    
    e chamar python makedao.py NomeDaClasse.dao
    Ele vai gerar as funções necessárias. Aí basta copiar e colar no arquivo .java
    que define a classe.

---
+++ src/orm/makedao.py
@@ -0,0 +1,60 @@
+#-*- coding: utf-8 -*-
+
+import os
+import sys
+
+INDENT = "    "
+
+def emitir_getter(classe, tabela, tipo, nome):
+    print "public static List<%s> por%s(%s %s)" \
+          % (classe,
+             nome.replace("_", " ").title().replace(" ", ""),
+             tipo,
+             nome)
+
+    print "{"
+    corpo = ("Session s = HibernateUtil.getSessionFactory().getCurrentSession();",
+           "s.beginTransaction();",
+           'Query q = s.createQuery("from %s where %s=?");' % (tabela, nome),
+           "List l = q.setString(0, %s).list();" % nome,
+           "List<%s> ret = new ArrayList<%s>(l.size());" % (classe, classe),
+           "for (Object o : l) ret.add((%s) o);" % classe,
+           "s.getTransaction().commit();",
+           "return ret;")
+
+    for linha in corpo:
+        print INDENT + linha
+
+    print "}"
+    print
+
+def emitir_salvar():
+    print "public void salvar()"
+    print "{"
+
+    corpo = ("Session s = HibernateUtil.getSessionFactory().getCurrentSession();",
+             "s.beginTransaction();",
+             "s.save(this);",
+             "s.getTransaction().commit();")
+
+    for linha in corpo:
+        print INDENT + linha
+
+    print "}"
+    print
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print >>sys.stderr, "Uso: %s <arquivo>.dao" % sys.argv[0]
+        sys.exit(1)
+
+    classe, _ = os.path.splitext(sys.argv[1])
+
+    with open(sys.argv[1], "r") as config:
+        tabela = config.readline().strip()
+        campos = map(str.split, config)
+
+    for tipo, nome in campos:
+        emitir_getter(classe, tabela, tipo, nome)
+
+    emitir_salvar()
