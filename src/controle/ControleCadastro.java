3182d97b6db91354e4a248269ca982177aa2bf91
Guilherme Pinto Goncalves <ra091429@snake.lab.ic.unicamp.br>
2011-11-30 11:32:47 -0200

    Cadastros agora guardam hashes das senhas.

--- src/controle/ControleCadastro.java
+++ src/controle/ControleCadastro.java
@@ -6,6 +6,9 @@ import java.util.Vector;
 import java.util.Set;
 import java.util.HashSet;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 import entidades.Cliente;
 
 public class ControleCadastro 
@@ -26,18 +29,50 @@ public class ControleCadastro
 		
         return instance;
     }	
-	
+
+    public String hashSenha(String senha)
+    {
+        byte byteData[];
+        MessageDigest md;
+
+        try {
+            md = MessageDigest.getInstance("SHA-256");
+            md.update(senha.getBytes());
+            byteData = md.digest();
+        } catch (NoSuchAlgorithmException e) {
+            // Não deveria acontecer
+            return null;
+        }
+
+        // Converte bytes para o formato hexadecimal.
+        // Fonte: http://www.mkyong.com/java/java-sha-hashing-example/
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < byteData.length; i++) {
+         sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+        }
+
+        System.out.println(sb.toString());
+        return sb.toString();
+    }
+
     public boolean cadastrarCliente(int id, int numero_cartao, String nome, String senha,
             String rg, String cpf, String email,
             HashSet<String> telefones)
     {
         //Cria o objeto cliente que será salvo no banco de dados
-        Cliente cliente = new Cliente(id, numero_cartao, nome, senha, rg, cpf, email, telefones);
-		
+        Cliente cliente = new Cliente(id,
+                                      numero_cartao,
+                                      nome,
+                                      hashSenha(senha),
+                                      rg,
+                                      cpf,
+                                      email,
+                                      telefones);
+
         //salvar o objeto cliente no banco de dados
         cliente.salvar();
 
         return true;
-    }
+        }
 
 }
